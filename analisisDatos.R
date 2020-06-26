da2a9a2fcf88824e84eb5c5323dd0dc6c79bc7fd
gema.gutierrez@urjc.es <gema.gutierrez@urjc.es>
2020-06-26 18:41:04 +0200

    algorithm

--- analisisDatos.R
+++ analisisDatos.R
@@ -1,4 +1,24 @@
-datos_correlacion <- read.csv2("datos-encuesta.csv", header = FALSE)
-source("calcular-pvalor.R")
-calcular_valor (datos_correlacion)
+datos_correlacion <- read.csv2("datos-encuestaKniberg.csv", header = FALSE)
+test <- matrix(1:72, nrow=3)
+rownames(test) <- c("Statistic-z", "pvalor", "Estimates-tau")
+colnames(test) <- c("Alto-Demo","Alto-Afili","Alto-Visio","Alto-Coach","Alto-Pace","Alto-Comm","Medio-Demo","Medio-Afili","Medio-Visio","Medio-Coach","Medio-Pace","Medio-Comm","MedioB-Demo","MedioB-Afili","MedioB-Visio","MedioB-Coach","MedioB-Pace","MedioB-Comm","Bajo-Demo","Bajo-Afili","Bajo-Visio","Bajo-Coach","Bajo-Pace","Bajo-Comm")
+cont_i <-1
+for (i in 1:4){ 
+  for (j in 5:10){ 
+    datos_correlacion[ ,i]
+    datos_correlacion[ ,j]
+    aux_test <- cor.test(datos_correlacion[ ,i],datos_correlacion[ ,j],method="kendall")
+    test[cont_i]<- aux_test$statistic 
+    cont_i <- cont_i+1
+    test[cont_i]<- aux_test$p.value 
+    cont_i <- cont_i+1
+    test[cont_i]<- aux_test$estimate 
+    cont_i <- cont_i+1
+  }
+}	
+test
+
+#datos_correlacion
+#source("calcular-pvalor.R")
+#calcular_valor (datos_correlacion)
 
