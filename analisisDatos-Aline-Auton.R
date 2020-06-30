847a01bcd321355f400008d1b996129cdf7b7039
gema.gutierrez@urjc.es <gema.gutierrez@urjc.es>
2020-06-30 22:03:28 +0200

    corrections

--- analisisDatos-Aline-Auton.R
+++ analisisDatos-Aline-Auton.R
@@ -1,4 +1,4 @@
-datos_correlacion <- read.csv2("datos-encuestaAutonom-Alineam.csv", header = FALSE)
+datos_correlacion <- read.csv2("datos-encuesta-Autonom-Alineam.csv", header = FALSE)
 test <- matrix(1:6, nrow=3)
 aux_test <- matrix(1:6, nrow=3)
 rownames(test) <- c("Statistic-z", "pvalor", "Estimates-tau")
@@ -11,7 +11,7 @@ test[cont_i]<- aux_test$p.value
 cont_i <- cont_i+1
 test[cont_i]<- aux_test$estimate 
 cont_i <- cont_i+1
-
+aux_test
 test
 pvalores = test[2,]
 p.adjust(pvalores, method="BY")
