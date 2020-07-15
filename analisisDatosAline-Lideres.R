c5a85764479e36c24effb94045eb5c8d3302d243
Gema <gema.gutierrez@urjc.es>
2020-07-15 11:16:22 +0200

    Commit

--- analisisDatosAline-Lideres.R
+++ analisisDatosAline-Lideres.R
@@ -1,3 +1,4 @@
+# todos
 datos_correlacion <- read.csv2("datos-encuestaLider.csv", header = FALSE)
 test <- matrix(1:18, nrow=3)
 aux_test <- matrix(1:18, nrow=3)
@@ -17,6 +18,49 @@ test
 pvalores = test[2,]
 p.adjust(pvalores, method="BY")
 
+
+# Solo pacesseting y commanding
+
+datos_correlacion <- read.csv2("datos-encuestaLider-Pac-Comm.csv", header = FALSE)
+test <- matrix(1:6, nrow=3)
+aux_test <- matrix(1:6, nrow=3)
+rownames(test) <- c("Statistic-z", "pvalor", "Estimates-tau")
+colnames(test) <- c("Alineam-Pace","Alineam-Comm")
+cont_i <-1
+for (j in 2:3){ 
+    aux_test <- cor.test(datos_correlacion[,1],datos_correlacion[,j],method="kendall")
+    test[cont_i]<- aux_test$statistic 
+    cont_i <- cont_i+1
+    test[cont_i]<- aux_test$p.value 
+    cont_i <- cont_i+1
+    test[cont_i]<- aux_test$estimate 
+    cont_i <- cont_i+1
+}
+test
+pvalores = test[2,]
+p.adjust(pvalores, method="BY")
+
+
+# Agrupacion: Flexibles y Autoritarios
+
+datos_correlacion <- read.csv2("datos-encuestaLider-Agrupados.csv", header = FALSE)
+test <- matrix(1:6, nrow=3)
+aux_test <- matrix(1:6, nrow=3)
+rownames(test) <- c("Statistic-z", "pvalor", "Estimates-tau")
+colnames(test) <- c("Flexibles","Autoritarios")
+cont_i <-1
+for (j in 2:3){ 
+    aux_test <- cor.test(datos_correlacion[,1],datos_correlacion[,j],method="kendall")
+    test[cont_i]<- aux_test$statistic 
+    cont_i <- cont_i+1
+    test[cont_i]<- aux_test$p.value 
+    cont_i <- cont_i+1
+    test[cont_i]<- aux_test$estimate 
+    cont_i <- cont_i+1
+}
+test
+pvalores = test[2,]
+p.adjust(pvalores, method="BY")
 
 
 
