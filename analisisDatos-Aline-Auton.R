38658692156b7185413a07ef6e8dbab477ff7fda
gema.gutierrez@urjc.es <gema.gutierrez@urjc.es>
2020-07-27 21:03:09 +0200

    Commit

--- analisisDatos-Aline-Auton.R
+++ analisisDatos-Aline-Auton.R
@@ -1,4 +1,5 @@
-datos_correlacion <- read.csv2("datos-encuesta-Autonom-Alineam.csv", header = FALSE)
+#datos_correlacion <- read.csv2("datos-encuesta-Autonom-Alineam.csv", header = FALSE)
+datos_correlacion <- read.csv2("datos-encuesta-Autonom-Alineam2.csv", header = FALSE)
 aux_test <- cor.test(datos_correlacion[, 1],datos_correlacion[,2],method="kendall")
 aux_test
 
